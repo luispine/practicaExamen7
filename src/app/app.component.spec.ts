78602e5871d2dbda8ce8ea0f54d37b843c9ec232
luiVaz <vazquezcedar@outlook.com>
2024-01-10 22:17:59 -0600

    cambios de nuevos ui

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'practicaExamen7' title`, () => {
+  it(`should havehe 'practicaExamen7' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('practicaExamen7');
